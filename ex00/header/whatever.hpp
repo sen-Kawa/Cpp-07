6b3515aea662ec70fcdb2b8bd83a5668e0f4045e
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-16 23:40:42 +0100

    min function template

--- ex00/header/whatever.hpp
+++ ex00/header/whatever.hpp
@@ -6,7 +6,13 @@
 template<typename T>
 T const	&max(T const &x, T const &y)
 {
-	return (x >= y ? x : y);
+	return (x > y ? x : y);
+}
+
+template<typename T>
+T const	&min(T const &x, T const &y)
+{
+	return (x < y ? x : y);
 }
 
 #endif
