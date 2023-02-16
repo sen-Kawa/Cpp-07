78e3ef4adbee9c92b6c34bbd69ca373b3ba5bebd
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-17 00:27:52 +0100

    function template for print, for third param of iter

--- ex01/header/iter.hpp
+++ ex01/header/iter.hpp
@@ -1,9 +1,18 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#include <iostream>
+
 template<typename T>
-void	iter(T *address, T length, void (*f)(T))
+void	iter(T *address, int length, void (*f)(T))
 {
+	return ;
+}
+
+template<typename T>
+void	print(T member)
+{
+	std::cout << member << std::endl;
 	return ;
 }
 
