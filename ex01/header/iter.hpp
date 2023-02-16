3e09a4c977f05edb132d215a350726abcfde4f6a
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-17 00:38:14 +0100

    starting to write tester

--- ex01/header/iter.hpp
+++ ex01/header/iter.hpp
@@ -4,8 +4,10 @@
 #include <iostream>
 
 template<typename T>
-void	iter(T *address, int length, void (*f)(T))
+void	iter(T *array, int length, void (*f)(T))
 {
+	for (int i = 0; i < length; i++)
+		f(array[i]);
 	return ;
 }
 
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -2,7 +2,21 @@
 
 int main(void)
 {
-	int	arr[5] = {2, 4, 6, 8, 10};
+	std::cout << "ITER TESTER\n" << std::endl;
+
+	std::cout << "Array of even numbers\n" << std::endl;
+	int	evens[5] = {2, 4, 6, 8, 10};
+
+	iter(evens, 5, print);
+
+	char	vowels[5] = {'a', 'e', 'i', 'o', 'u'};
+
+	iter(vowels, 5, print);
+
+	std::string	planet_type[4] = {"Giant planet", "Ice giant", "Mesoplanet", "Planetar"};
+
+	iter(planet_type, 5, print);
+
 	return (0);
 }
 
