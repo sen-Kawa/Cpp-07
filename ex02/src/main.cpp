0547fbfb75323052fcf15dc636e149d001ee4e5f
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-22 04:43:41 +0100

    tests

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -5,18 +5,21 @@
 
 int main(int, char**)
 {
+	std::cout << YELLOW << ">> Instantiating..." << DEF << std::endl;
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
+	std::cout << YELLOW << ">> Subscript operator [] overload test" << DEF << std::endl;
     for (int i = 0; i < MAX_VAL; i++)
     {
         const int value = rand();
         numbers[i] = value;
         mirror[i] = value;
     }
-  //SCOPE
     {
+		std::cout << YELLOW << ">> Assignment overload test" << DEF << std::endl;
         Array<int> tmp = numbers;
+		std::cout << YELLOW << ">> Copy constructor test" << DEF << std::endl;
         Array<int> test(tmp);
     }
 
@@ -30,6 +33,7 @@ int main(int, char**)
     }
     try
     {
+		std::cout << YELLOW << ">> Exception borders test" << DEF << std::endl;
         numbers[-2] = 0;
     }
     catch(const std::exception& e)
@@ -38,6 +42,7 @@ int main(int, char**)
     }
     try
     {
+		std::cout << YELLOW << ">> Exception borders test" << DEF << std::endl;
         numbers[MAX_VAL] = 0;
     }
     catch(const std::exception& e)
